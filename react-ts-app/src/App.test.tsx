4839f8c358cf6874abbaea0b38dbd3ec901c4e4f
tat.bandelikova <tat.bandelikova@gmail.com>
2023-03-26 23:08:37 +0300

    feat: add test case

--- react-ts-app/src/App.test.tsx
+++ react-ts-app/src/App.test.tsx
@@ -4,6 +4,7 @@ import { describe, it } from 'vitest';
 
 import App from './App';
 import Card from './components/Card';
+import CardUser from './components/CardUser';
 
 describe('App', () => {
   it('Should have H1 headline', () => {
@@ -19,6 +20,17 @@ describe('Card', () => {
   });
   it('Card should have a title', () => {
     render(<Card id={0} title={''} img={''} price={''} text={''} tags={[]} />);
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+});
+
+describe('CardUser', () => {
+  it('Images should have alt-attribute', () => {
+    render(<CardUser name={''} birthday={''} gender={''} location={''} img={''} />);
+    expect(screen.getByRole('img')).toHaveAttribute('alt');
+  });
+  it('Card should have a title', () => {
+    render(<CardUser name={''} birthday={''} gender={''} location={''} img={''} />);
     expect(screen.getByRole('heading')).toBeInTheDocument();
   });
 });
